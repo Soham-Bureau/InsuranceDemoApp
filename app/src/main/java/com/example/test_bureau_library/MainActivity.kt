a82c0fde129f864c23183a57537dd17659eb38b9
shah <shahhimanshu02@gmail.com>
2021-04-29 17:49:43 +0530

    - Demo code for url filtering is added

--- app/src/main/java/com/example/test_bureau_library/MainActivity.kt
+++ app/src/main/java/com/example/test_bureau_library/MainActivity.kt
@@ -1,20 +1,28 @@
+@file:Suppress("DEPRECATED_IDENTITY_EQUALS")
+
 package com.example.test_bureau_library
 
 import android.Manifest
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.ServiceInfo
 import android.os.Bundle
+import android.provider.Settings
+import android.view.accessibility.AccessibilityManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.bureau.`interface`.ApplicationFilterInterface
-import com.bureau.`interface`.CallFilterInterface
-import com.bureau.`interface`.SIMFilterInterface
-import com.bureau.`interface`.SMSFilterInterface
+import com.bureau.`interface`.*
 import com.bureau.helpers.AllInstalledAppsHelper
-import com.bureau.models.packageDetectorHelper.AllInstalledAppResponse
+import com.bureau.services.ASUrl
 import com.bureau.services.ValidationService
 import com.bureau.utils.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,15 +33,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // Firstly, we check READ_CALL_LOG permission
         if (ContextCompat.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.READ_CALL_LOG
-                ) !== PackageManager.PERMISSION_GRANTED
+                this@MainActivity,
+                Manifest.permission.READ_CALL_LOG
+            ) !== PackageManager.PERMISSION_GRANTED
         ) {
             // We do not have this permission. Let's ask the user
             ActivityCompat.requestPermissions(
-                    this@MainActivity,
-                    arrayOf(Manifest.permission.READ_CALL_LOG),
-                    MY_PERMISSIONS_REQUEST_READ_CALL_LOG
+                this@MainActivity,
+                arrayOf(Manifest.permission.READ_CALL_LOG),
+                MY_PERMISSIONS_REQUEST_READ_CALL_LOG
             )
         }
         initRequestPermission()
@@ -78,9 +86,9 @@ class MainActivity : AppCompatActivity() {
             override fun maliciousApps(list: ArrayList<String>) {
                 val commaSeparatedString = list.joinToString(separator = ", ")
                 Toast.makeText(
-                        this@MainActivity,
-                        "Malicious Apps --> $commaSeparatedString ",
-                        Toast.LENGTH_LONG
+                    this@MainActivity,
+                    "Malicious Apps --> $commaSeparatedString ",
+                    Toast.LENGTH_LONG
                 ).show()
             }
 
@@ -94,25 +102,51 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        AllInstalledAppsHelper().initAllInstalledApps(this, true, object : ApplicationFilterInterface{
-            override fun maliciousApps(list: ArrayList<String>) {
-                Toast.makeText(
+        AllInstalledAppsHelper().initAllInstalledApps(
+            this,
+            true,
+            object : ApplicationFilterInterface {
+                override fun maliciousApps(list: ArrayList<String>) {
+                    Toast.makeText(
                         this@MainActivity,
                         "Malicious Apps Activity --> $list ",
                         Toast.LENGTH_LONG
-                ).show()
-            }
+                    ).show()
+                }
 
-            override fun safeApp(appName: String) {
-                Toast.makeText(
+                override fun safeApp(appName: String) {
+                    Toast.makeText(
                         this@MainActivity,
                         "safeApp Apps Activity --> $appName ",
                         Toast.LENGTH_LONG
-                ).show()
+                    ).show()
+                }
+            })
+
+        //url filter callbacks
+        ASUrl.initCallbacks(object : UrlFilterInterface {
+            override fun urlDetected(url: String) {
+                Toast.makeText(this@MainActivity, "urlDetected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun safeUrl(url: String) {
+                Toast.makeText(this@MainActivity, "safeUrl", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun unSafeUrl(url: String) {
+                Toast.makeText(this@MainActivity, "unSafeUrl", Toast.LENGTH_SHORT).show()
             }
         })
-
-
+        switch_accessibility_service.apply {
+            isChecked = isAccessibilityServiceEnabled(this@MainActivity, ASUrl::class.java)
+            setOnCheckedChangeListener { button, boolean ->
+                if (button.isPressed && boolean) {
+                    if (!isAccessibilityServiceEnabled(this@MainActivity, ASUrl::class.java)){
+                        startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+                    }
+                }
+            }
+        }
     }
 
     private fun initRequestPermission() {
@@ -131,7 +165,11 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         if (marshMellowHelper != null) {
             marshMellowHelper!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
@@ -140,9 +178,17 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission granted!
                     // check READ_PHONE_STATE permission only when READ_CALL_LOG is granted
-                    if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.READ_PHONE_STATE) !== PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.READ_PHONE_STATE
+                        ) !== PackageManager.PERMISSION_GRANTED
+                    ) {
                         // We do not have this permission. Let's ask the user
-                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_PHONE_STATE), MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.READ_PHONE_STATE),
+                            MY_PERMISSIONS_REQUEST_READ_PHONE_STATE
+                        )
                     }
                 }
             }
@@ -150,9 +196,17 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission granted!
                     // check PROCESS_OUTGOING_CALLS permission only when READ_PHONE_STATE is granted
-                    if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.PROCESS_OUTGOING_CALLS) !== PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.PROCESS_OUTGOING_CALLS
+                        ) !== PackageManager.PERMISSION_GRANTED
+                    ) {
                         // We do not have this permission. Let's ask the user
-                        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.PROCESS_OUTGOING_CALLS), MY_PERMISSIONS_REQUEST_PROCESS_OUTGOING_CALLS)
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.PROCESS_OUTGOING_CALLS),
+                            MY_PERMISSIONS_REQUEST_PROCESS_OUTGOING_CALLS
+                        )
                     }
                 }
             }
